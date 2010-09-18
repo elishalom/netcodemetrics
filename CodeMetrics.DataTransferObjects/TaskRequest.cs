eb1c1b02314b45506f28c308c03e0fbfae7cb742
Elisha <elishalom@users.noreply.github.com>
2010-09-18 19:33:07 +0200

    Made some performance improvments

---
+++ CodeMetrics.DataTransferObjects/TaskRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CodeMetrics.DataTransferObjects
+{
+    [DataContract]
+    public class TaskRequest
+    {
+        [DataMember]
+        public Guid ID { get; set; }
+
+        [DataMember]
+        public string MethodBody { get; set; }
+    }
+
+    [DataContract]
+    public class TaskResult
+    {
+        [DataMember]
+        public Guid ID { get; set; }
+
+        [DataMember]
+        public int ComplexityValue { get; set; }
+    }
+}
---
+++ CodeMetrics.WorkerServices/CodeMetricsService.cs
@@ -0,0 +1,16 @@
+﻿using CodeMetrics.DataTransferObjects;
+
+namespace CodeMetrics.WorkerServices
+{
+    public class CodeMetricsService : ICodeMetricsService
+    {
+        public TaskResult PerformTask(TaskRequest task)
+        {
+            return new TaskResult()
+                {
+                    ID = task.ID,
+                    ComplexityValue = 2
+                };
+        }
+    }
+}
