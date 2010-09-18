468ecde5d62d667b0dea38b11b6a8a213f050a7a
Elisha <Elisha@localhost>
2010-09-18 17:06:38 +0200

    Refactored view

---
+++ CodeMetrics.Addin/ComplexityViewHost.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using CodeMetrics.Calculators;
+
+namespace CodeMetrics.Addin
+{
+    public class ComplexityViewHost : UserControl
+    {
+        private readonly ElementHost wpfHost;
+
+        public ComplexityViewHost(IComplexity complexity)
+        {
+            this.Size = new Size(30, 20);
+
+            wpfHost = new ElementHost
+                               {
+                                   Dock = DockStyle.Fill,
+                                   Child = new ComplexityView(complexity)
+                               };
+            Controls.Add(wpfHost);
+
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            wpfHost.Refresh();
+            base.OnPaint(e);
+        }
+    }
+}
