f682a286de11c124426be7db7d1558a7593cdf68
jirkapok <jirkapok@localhost>
2013-12-15 13:29:20 +0100

    Added missing properties visitor file.

---
+++ CodeMetrics.Parsing/PropertyVisitor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using ICSharpCode.NRefactory.CSharp;
+
+namespace CodeMetrics.Parsing
+{
+    public class PropertyVisitor : DepthFirstAstVisitor, IMethodsVisitor
+    {
+        private readonly List<IMethod> properties;
+
+        public PropertyVisitor()
+        {
+            properties = new List<IMethod>();
+        }
+
+        public IEnumerable<IMethod> Methods 
+        {
+            get
+            {
+                return properties;
+            }
+        }
+
+        public override void VisitPropertyDeclaration(PropertyDeclaration propertyDeclaration)
+        {
+            base.VisitPropertyDeclaration(propertyDeclaration);
+
+            AddAccessorMethod(propertyDeclaration.Getter);
+            AddAccessorMethod(propertyDeclaration.Setter);
+        }
+
+        private void AddAccessorMethod(Accessor accessor)
+        {
+            var accessorStartLocation = accessor.StartLocation.AsLocation();
+            var accessorEndLocation = accessor.EndLocation.AsLocation();
+            properties.Add(new Method(accessorStartLocation, accessorStartLocation, accessorEndLocation));
+        }
+    }
+}
