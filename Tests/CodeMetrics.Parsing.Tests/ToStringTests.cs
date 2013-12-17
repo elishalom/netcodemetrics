0c983cf6966fac1b83df09e8adee582b8939cca0
jirkapok <jirkapok@localhost>
2013-12-18 00:23:45 +0100

    Added ToStrings UnitTests.

---
+++ Tests/CodeMetrics.Parsing.Tests/ToStringTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace CodeMetrics.Parsing.Tests
+{
+    [TestFixture]
+    internal class ToStringTests
+    {
+        [Test]
+        public void ValidLocationToString_ReturnsLineAndColumn()
+        {
+            var location = new Location(1, 2);
+            string locationText = location.ToString();
+            Assert.AreEqual("Location:1,2", locationText, "Location ToString method should return line and column values");
+        }
+
+        [Test]
+        public void ValidMethodToString_ReturnsAllConcatenatedLocations()
+        {
+            var declaration = new Location(1, 2);
+            var start = new Location(2, 3);
+            var end = new Location(4, 5);
+            var method = new Method(declaration, start, end);
+            string methodText = method.ToString();
+            Assert.AreEqual("Method:[1,2-2,3-4,5]", methodText, "Method ToString method should return line and column values");
+        }
+    }
+}
