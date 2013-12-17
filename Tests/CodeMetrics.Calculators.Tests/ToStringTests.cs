565b6fa8895bf59adc96400abd3034ecc2be8fed
jirkapok <jirkapok@localhost>
2013-12-18 00:23:45 +0100

    Added ToStrings UnitTests.

---
+++ Tests/CodeMetrics.Calculators.Tests/ToStringTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeMetrics.Parsing;
+using NUnit.Framework;
+
+namespace CodeMetrics.Calculators.Tests
+{
+    [TestFixture]
+    internal class ToStringTests
+    {
+        [Test]
+        public void ComplexityToString_ReturnsValue()
+        {
+            var complexity = new Complexity(5);
+            string complexityText = complexity.ToString();
+            Assert.AreEqual("Complexity:5", complexityText, "Complexity ToString should return formated value.");
+        }
+    }
+}
