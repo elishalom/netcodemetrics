f6c10488a7927b387fe4ac3027652a26c2622b42
Jiri Pokorny <j.pok@post.cz>
2015-11-23 23:25:39 +0100

    Added new option for Maximum complexity collor.

---
+++ CodeMetrics.Adornments/Options/ColorSelection.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CodeMetrics.Options
+{
+    partial class ColorSelection
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.editButton = new System.Windows.Forms.Button();
+            this.previewPanel = new System.Windows.Forms.Panel();
+            this.rangeColorDialog = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(35, 0);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(27, 23);
+            this.editButton.TabIndex = 4;
+            this.editButton.Text = "...";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.ChangeColorButtonClick);
+            // 
+            // previewPanel
+            // 
+            this.previewPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.previewPanel.Location = new System.Drawing.Point(0, 0);
+            this.previewPanel.Name = "previewPanel";
+            this.previewPanel.Size = new System.Drawing.Size(30, 23);
+            this.previewPanel.TabIndex = 5;
+            this.previewPanel.DoubleClick += new System.EventHandler(this.ChangeColorButtonClick);
+            // 
+            // ColorSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.previewPanel);
+            this.Name = "ColorSelection";
+            this.Size = new System.Drawing.Size(64, 23);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Panel previewPanel;
+        private System.Windows.Forms.ColorDialog rangeColorDialog;
+    }
+}
---
+++ CodeMetrics.Adornments/Options/ColorSelection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CodeMetrics.Options
+{
+    internal partial class ColorSelection : UserControl
+    {
+        public event EventHandler<ColorChangedEventArgs> SelectedColorChanged; 
+
+        internal Color SelectedColor
+        {
+            get
+            {
+                return this.previewPanel.BackColor;
+            }
+            set
+            {
+                this.previewPanel.BackColor = value;
+            }
+        }
+
+        public ColorSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeColorButtonClick(object sender, EventArgs e)
+        {
+            this.rangeColorDialog.Color = this.SelectedColor;
+
+            if (this.rangeColorDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.SelectedColor = this.rangeColorDialog.Color;
+                FireColorChanged();
+            }
+        }
+
+        private void FireColorChanged()
+        {
+            if (SelectedColorChanged != null)
+            {
+                var args = new ColorChangedEventArgs() { NewColor = this.SelectedColor };
+                SelectedColorChanged(this, args);
+            }
+        }
+    }
+}
