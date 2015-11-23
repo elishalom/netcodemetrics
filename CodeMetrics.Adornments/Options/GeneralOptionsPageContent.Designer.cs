d8143bf68d6fa754b032bc09dc24293950842cb9
Jiri Pokorny <j.pok@post.cz>
2015-11-23 22:32:55 +0100

    Added Minimum Color option to options dialog.

--- CodeMetrics.Adornments/Options/GeneralOptionsPageContent.Designer.cs
+++ CodeMetrics.Adornments/Options/GeneralOptionsPageContent.Designer.cs
@@ -28,22 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.thresholdTextbox = new System.Windows.Forms.TextBox();
+            this.thresholdLabel = new System.Windows.Forms.Label();
+            this.mainToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.minimumColorLabel = new System.Windows.Forms.Label();
+            this.rangeColorDialog = new System.Windows.Forms.ColorDialog();
+            this.minimumColorPreview = new System.Windows.Forms.Panel();
+            this.minimumColorButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // thresholdTextbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(18, 22);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.TextBoxTextChanged);
+            this.thresholdTextbox.Location = new System.Drawing.Point(164, 31);
+            this.thresholdTextbox.Name = "thresholdTextbox";
+            this.thresholdTextbox.Size = new System.Drawing.Size(48, 20);
+            this.thresholdTextbox.TabIndex = 0;
+            this.mainToolTip.SetToolTip(this.thresholdTextbox, "Define minum number of fomplexity to show.\r\nHas to be a number higher or equals t" +
+        "han 1.");
+            this.thresholdTextbox.TextChanged += new System.EventHandler(this.TextBoxTextChanged);
+            // 
+            // thresholdLabel
+            // 
+            this.thresholdLabel.AutoSize = true;
+            this.thresholdLabel.Location = new System.Drawing.Point(44, 34);
+            this.thresholdLabel.Name = "thresholdLabel";
+            this.thresholdLabel.Size = new System.Drawing.Size(104, 13);
+            this.thresholdLabel.TabIndex = 1;
+            this.thresholdLabel.Text = "Minimum Complexity:";
+            // 
+            // minimumColorLabel
+            // 
+            this.minimumColorLabel.AutoSize = true;
+            this.minimumColorLabel.Location = new System.Drawing.Point(18, 62);
+            this.minimumColorLabel.Name = "minimumColorLabel";
+            this.minimumColorLabel.Size = new System.Drawing.Size(130, 13);
+            this.minimumColorLabel.TabIndex = 2;
+            this.minimumColorLabel.Text = "Minimum complexity Color:";
+            // 
+            // minimumColorPreview
+            // 
+            this.minimumColorPreview.Location = new System.Drawing.Point(164, 57);
+            this.minimumColorPreview.Name = "minimumColorPreview";
+            this.minimumColorPreview.Size = new System.Drawing.Size(30, 23);
+            this.minimumColorPreview.TabIndex = 3;
+            // 
+            // minimumColorButton
+            // 
+            this.minimumColorButton.Location = new System.Drawing.Point(200, 57);
+            this.minimumColorButton.Name = "minimumColorButton";
+            this.minimumColorButton.Size = new System.Drawing.Size(64, 23);
+            this.minimumColorButton.TabIndex = 0;
+            this.minimumColorButton.Text = "Edit";
+            this.minimumColorButton.UseVisualStyleBackColor = true;
+            this.minimumColorButton.Click += new System.EventHandler(this.MinimumColorButtonClick);
             // 
             // GeneralOptionsPageContent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.minimumColorButton);
+            this.Controls.Add(this.minimumColorPreview);
+            this.Controls.Add(this.minimumColorLabel);
+            this.Controls.Add(this.thresholdLabel);
+            this.Controls.Add(this.thresholdTextbox);
             this.Name = "GeneralOptionsPageContent";
             this.Size = new System.Drawing.Size(688, 350);
             this.ResumeLayout(false);
@@ -53,6 +101,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox thresholdTextbox;
+        private System.Windows.Forms.ToolTip mainToolTip;
+        private System.Windows.Forms.Label thresholdLabel;
+        private System.Windows.Forms.Label minimumColorLabel;
+        private System.Windows.Forms.ColorDialog rangeColorDialog;
+        private System.Windows.Forms.Panel minimumColorPreview;
+        private System.Windows.Forms.Button minimumColorButton;
     }
 }
