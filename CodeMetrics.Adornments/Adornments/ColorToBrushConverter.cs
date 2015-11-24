e15d43fcff8716e7e861649e951af0c1b6da0f54
Jiri Pokorny <j.pok@post.cz>
2015-11-24 13:03:46 +0100

    Added ColorToBrushConverter and its tests.

---
+++ CodeMetrics.Adornments/Adornments/ColorToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CodeMetrics.Adornments
+{
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color)
+            {
+                Color color = (Color)value;
+                return new SolidColorBrush(color);
+            }
+
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/CodeMetrics.Adornments.Tests/ColorToBrushConverterTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Windows.Media;
+using NUnit.Framework;
+
+namespace CodeMetrics.Adornments.Tests
+{
+    [TestFixture]
+    public class ColorToBrushConverterTests
+    {
+        [Test]
+        public void Null_Convert_ReturnTransparentBrush()
+        {
+            var brush = this.Convert(null);
+            Assert.That(brush, Is.EqualTo(Brushes.Transparent));
+        }
+
+        [Test]
+        public void Object_Convert_ReturnTransparentBrush()
+        {
+            var brush = this.Convert(new object());
+            Assert.That(brush, Is.EqualTo(Brushes.Transparent));
+        }
+
+        [Test]
+        public void GreenColor_Convert_ReturnGreenBrush()
+        {
+            Color expected = Colors.Green;
+            var brush = this.Convert(expected);
+            Assert.That(brush.Color, Is.EqualTo(expected));
+        }
+
+        private SolidColorBrush Convert(object toConvert)
+        {
+            var converter = new ColorToBrushConverter();
+            return (SolidColorBrush)converter.Convert(toConvert, typeof(Color), null, CultureInfo.InvariantCulture);
+        }
+    }
+}
