eef5c8213fa0c832d90bdc2c57fe083441494671
elishalom <eli.shalom@gmail.com>
2017-04-29 20:30:23 +0300

    Updated extenrsion supported Visual Studio versions - Added VS 2017 and removed VS 2010.

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
